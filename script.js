b9af2c8943d54e81028bd0f6a19c3e1c6511080e
Richard Davidian <127055926+rhdavidian@users.noreply.github.com>
2024-04-01 09:30:48 -0500

    Remove markCell logic referencing the value of the object in array, instead created the array referencing the values themselves so it's not a 2d array of objects. Create a whoseTurn function to check turns.

--- script.js
+++ script.js
@@ -6,28 +6,74 @@ function Gameboard() {
     for (let i = 0; i < rows; i++){
         board[i] = [];
         for (let j = 0; j < columns; j++)
-        board[i].push(Cell());
+        board[i].push(Cell().value);
     }
 
     const getBoard = () => board;
 
+    const renderBoard = () => 
+           console.table(board);
+
     //function to mark a cell with an X or O as a token
     const markCell = (row, column, token) =>  {
-        if (board[row][column].value === '') {
-            board[row][column].value = token;
-        } else {
-            alert('That spot is taken');
-        }
+        // token = game.getActivePlayer().name;
+        // console.log(`That was ${token}\'s turn.`)
+        token = game.getActivePlayer().token;
+        if (board[row][column] === '-') {
+            board[row][column] = token;
+            game.switchPlayer();
+        } else if (board[row][column] === 'X' || board[row][column].value === 'O'){
+            alert('That spot is taken, choose another');
+        };
+        console.log(renderBoard());
     };
-    
-    return { getBoard, markCell };
+    return { getBoard, markCell, renderBoard };
 };
 
-//use this function to return functions addToken (X or O)
-//as well as getValue. Value is 0 just for now
 function Cell() {
-    let value = '';
+    let value = '-';
     return { value };
 }
 
-const array = Gameboard();
+// function Cell() {
+//     let value = '';
+//     const addToken = (token) => {
+//         value = token;
+//     }
+//     const getValue = () => value;
+
+//     return { getValue, addToken };
+// }
+
+function Gamecontroller (
+    playerOneName = 'Player One',
+    playerTwoName = 'Player Two'
+) {
+
+    const players = [
+        {
+            name: playerOneName,
+            token: 'X'
+        },
+        {
+            name: playerTwoName,
+            token: 'O'
+        }
+    ]
+
+    let activePlayer = players[0];
+    const switchPlayer = () => {
+       activePlayer = activePlayer === players[0] ? players[1] : players[0]; 
+    }
+    const getActivePlayer = () => activePlayer;
+    const whoseTurn = () => console.log(`It is ${activePlayer.name}\'s turn.`);
+
+    return { getActivePlayer, switchPlayer, whoseTurn }
+
+    //include functions to switch players
+    //include way to name players
+}
+
+const board = Gameboard();
+const game = Gamecontroller();
+
