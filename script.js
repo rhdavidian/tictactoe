acaeac85c7463699af408a66cd37b8d730a67006
Richard Davidian <127055926+rhdavidian@users.noreply.github.com>
2024-04-02 09:33:43 -0500

    Create function to announce a winner for a winning row. renamed a variable or two for clarity

--- script.js
+++ script.js
@@ -10,22 +10,23 @@ function Gameboard() {
     }
 
     const getBoard = () => board;
-
     const renderBoard = () => 
            console.table(board);
 
     //function to mark a cell with an X or O as a token
     const markCell = (row, column, token) =>  {
-        // token = game.getActivePlayer().name;
-        // console.log(`That was ${token}\'s turn.`)
+        const player = game.getActivePlayer().name;
         token = game.getActivePlayer().token;
+
         if (board[row][column] === '-') {
             board[row][column] = token;
-            game.switchPlayer();
-        } else if (board[row][column] === 'X' || board[row][column].value === 'O'){
+            console.log(`That was ${player}\'s turn. Very good.`)
+            game.announceWinner();
+        } else if (board[row][column] === 'X' || board[row][column] === 'O'){
             alert('That spot is taken, choose another');
         };
         console.log(renderBoard());
+        game.announceWinner();
     };
     return { getBoard, markCell, renderBoard };
 };
@@ -35,15 +36,6 @@ function Cell() {
     return { value };
 }
 
-// function Cell() {
-//     let value = '';
-//     const addToken = (token) => {
-//         value = token;
-//     }
-//     const getValue = () => value;
-
-//     return { getValue, addToken };
-// }
 
 function Gamecontroller (
     playerOneName = 'Player One',
@@ -68,9 +60,34 @@ function Gamecontroller (
     const getActivePlayer = () => activePlayer;
     const whoseTurn = () => console.log(`It is ${activePlayer.name}\'s turn.`);
 
-    return { getActivePlayer, switchPlayer, whoseTurn }
+    const announceWinner = () => {
+        const row = board.getBoard();
+        
+        //return elements in each column to check for 3 in a row, work in progress. try moving the loop
+        row.map((value, index) => { 
+            for (index = 0; i < 3; i++)
+             console.log(value[index]);
+        });
 
-    //include functions to switch players
+        // for (let i = 0; i < 3; i++){
+            // row.forEach((element) => console.log(element.join('')))
+
+            //announce winner if row wins
+            row.forEach((element) =>  {
+                if (element.join('') === 'XXX') {
+                    alert('Player One Wins!')
+                } else if (element.join('') === 'OOO') {
+                    alert('Player Two Wins!')
+                } else {
+                    return;
+                }
+            });
+            row.map((element) => console.log(element));
+        // }
+    }
+
+    return { getActivePlayer, switchPlayer, whoseTurn, announceWinner }
+
     //include way to name players
 }
 
