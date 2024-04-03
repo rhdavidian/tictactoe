fb96f053bc0ac6129113444cb36a4168718e1378
Richard Davidian <127055926+rhdavidian@users.noreply.github.com>
2024-04-03 12:22:50 -0500

    Finish the logic to announce a winner and adjusted the winning message to take a variable so it's not generic Player One or Player Two.

--- script.js
+++ script.js
@@ -21,12 +21,13 @@ function Gameboard() {
         if (board[row][column] === '-') {
             board[row][column] = token;
             console.log(`That was ${player}\'s turn. Very good.`)
+            renderBoard();
             game.announceWinner();
+            game.switchPlayer();
         } else if (board[row][column] === 'X' || board[row][column] === 'O'){
             alert('That spot is taken, choose another');
         };
-        console.log(renderBoard());
-        game.announceWinner();
+        // console.log(renderBoard());
     };
     return { getBoard, markCell, renderBoard };
 };
@@ -35,7 +36,6 @@ function Cell() {
     let value = '-';
     return { value };
 }
-
 
 function Gamecontroller (
     playerOneName = 'Player One',
@@ -56,35 +56,59 @@ function Gamecontroller (
     let activePlayer = players[0];
     const switchPlayer = () => {
        activePlayer = activePlayer === players[0] ? players[1] : players[0]; 
+       console.log(`It is now ${activePlayer.name}\'s turn.`)
     }
     const getActivePlayer = () => activePlayer;
     const whoseTurn = () => console.log(`It is ${activePlayer.name}\'s turn.`);
 
     const announceWinner = () => {
         const row = board.getBoard();
-        
-        //return elements in each column to check for 3 in a row, work in progress. try moving the loop
-        row.map((value, index) => { 
-            for (index = 0; i < 3; i++)
-             console.log(value[index]);
+        const diagonalOne = [];
+        const diagonalTwo = [];
+
+        //check diagonals
+        for (let i = 0; i < 3; i++){
+            diagonalOne.push(row[i][i]);
+            diagonalTwo.push(row[2-i][i]);
+        }
+        if (diagonalOne.join('') === 'XXX' || diagonalTwo.join('') === 'XXX') {
+            alert(`${players[0].name} Wins!`)
+        } else if (diagonalOne.join('') === 'OOO' || diagonalTwo.join('') === 'OOO'){
+            alert(`${players[1].name} Wins!`)
+        }
+
+        //check rows
+        row.forEach((element) => {
+            if (element.join('') === 'XXX') {
+                alert(`${players[0].name} Wins!`)
+            } else if (element.join('') === 'OOO') {
+                alert(`${players[1].name} Wins!`)
+            }
         });
 
-        // for (let i = 0; i < 3; i++){
-            // row.forEach((element) => console.log(element.join('')))
-
-            //announce winner if row wins
-            row.forEach((element) =>  {
-                if (element.join('') === 'XXX') {
-                    alert('Player One Wins!')
-                } else if (element.join('') === 'OOO') {
-                    alert('Player Two Wins!')
-                } else {
-                    return;
-                }
-            });
-            row.map((element) => console.log(element));
-        // }
-    }
+        // //check columns 
+        const colArray1 = [];
+        const colArray2 = [];
+        const colArray3 = [];
+            for (let i = 0; i < 3; i++){
+                colArray1.push(row[i][0]);
+                var column1 = colArray1.join('');
+            };     
+            for (let i = 0; i < 3; i++){
+                colArray2.push(row[i][1]);
+                var column2 = colArray2.join('');
+            };
+            for (let i = 0; i < 3; i++){
+                colArray3.push(row[i][2]);
+                var column3 = colArray3.join('');
+            };
+        
+        if (column1 === 'XXX' || column2 === 'XXX' || column3 === 'XXX'){
+            alert(`${players[0].name} Wins!`)
+        } else if (column1 === 'OOO' || column2 === 'OOO' || column3 === 'OOO'){
+            alert(`${players[1].name} Wins!`)
+        }
+    };
 
     return { getActivePlayer, switchPlayer, whoseTurn, announceWinner }
 
